681a00116a6db55128156515abf15f07943c2a29
piaste <git@piaste.com>
2026-03-18 16:23:44 +0100

    Improve build file with version

--- build.fsx
+++ build.fsx
@@ -2,14 +2,15 @@
 
 open LSLib.LS
 open System.IO
+open System.Xml.Linq
 
 
 //cleanup old .loca files
-Directory.GetFiles("./Home Brew - Comprehensive Reworks/Localization/English/", "*.loca")
+do Directory.GetFiles("./Home Brew - Comprehensive Reworks/Localization/English/", "*.loca")
 |> Array.iter File.Delete
 
 // generate new .loca files
-Directory.GetFiles("./Home Brew - Comprehensive Reworks/Localization/English/", "*.xml")
+do Directory.GetFiles("./Home Brew - Comprehensive Reworks/Localization/English/", "*.xml")
 |> Array.map System.IO.Path.GetFullPath
 |> Array.iter (fun f ->    
     LocaUtils.Save(
@@ -19,13 +20,39 @@ Directory.GetFiles("./Home Brew - Comprehensive Reworks/Localization/English/", 
     )
 )
 
+// get mod version from `meta.lsx`
+
+
+let version64 =
+    // intentionally unsafe, will crash if it can't read the version
+    "./Home Brew - Comprehensive Reworks/Mods/Home Brew - Comprehensive Reworks - Lore Texts/meta.lsx"
+    |> XDocument.Load    
+    |> _.Descendants()
+        |> Seq.find (fun n -> 
+            n.Name = XName.Get "node" 
+            && n.Attribute(XName.Get "id").Value = "ModuleInfo")
+    |> _.Elements()
+        |> Seq.find (fun a -> 
+            a.Name = XName.Get "attribute" 
+            && a.Attribute(XName.Get "id").Value = "Version64")
+    
+    |> _.Attribute(XName.Get "value").Value
+    |> System.Int64.Parse
+    |> LSLib.LS.PackedVersion.FromInt64
+    |> fun pv -> sprintf "%i.%i.%i.%i" pv.Major pv.Minor pv.Revision pv.Build
+
 // build package
-Packager().CreatePackage(
-    packagePath = System.IO.Path.GetFullPath "./Home Brew - Comprehensive Reworks - Lore Text.pak",
-    inputPath = System.IO.Path.GetFullPath "./Home Brew - Comprehensive Reworks/" ,
-    build = new PackageBuildData(
-    Version = Enums.PackageVersion.V18,
-    Compression = CompressionMethod.LZ4,
-    Priority = 0uy
-)
-).Wait()
+let fileName = "Home Brew - Comprehensive Reworks - Lore Text-" + version64 + ".pak"
+let outputPak = System.IO.Path.GetFullPath $"./{fileName}"
+do File.Delete outputPak
+do Packager().CreatePackage(
+        packagePath = outputPak,
+        inputPath = System.IO.Path.GetFullPath "./Home Brew - Comprehensive Reworks/" ,
+        build = new PackageBuildData(
+            Version = Enums.PackageVersion.V18,
+            Compression = CompressionMethod.LZ4,
+            Priority = 0uy
+        )
+    ).Wait()
+
+System.Console.WriteLine $"Generated {outputPak}"
